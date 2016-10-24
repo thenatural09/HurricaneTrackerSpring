20b12133234454ae6f0a6d3b6dec1e72b7717ed6
Troy <tward4@tulane.edu>
2016-10-24 10:23:19 -0400

    Add filtering and searching

--- src/main/java/com/company/HurricaneRepository.java
+++ src/main/java/com/company/HurricaneRepository.java
@@ -2,8 +2,16 @@ package com.company;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 /**
  * Created by Troy on 10/21/16.
  */
 public interface HurricaneRepository extends CrudRepository<Hurricane,Integer> {
+    List<Hurricane> findByCategory(int category);
+    List<Hurricane> findByLocation(String location);
+    List<Hurricane> findByNameContainingOrLocationContaining(String name,String location);
+
+    Hurricane findFirstByLocation(String location);
+    int countByCategory(int category);
 }
--- src/main/java/com/company/HurricaneTrackerController.java
+++ src/main/java/com/company/HurricaneTrackerController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by Troy on 10/21/16.
@@ -17,8 +18,17 @@ public class HurricaneTrackerController {
     HurricaneRepository hurricanes;
 
     @RequestMapping(path = "/",method = RequestMethod.GET)
-    public String home(Model model) {
-        Iterable<Hurricane> hlist = hurricanes.findAll();
+    public String home(Model model, Integer category,String search) {
+        List<Hurricane> hlist;
+        if (category!= null) {
+            hlist = hurricanes.findByCategory(category);
+        }
+        else if (search != null) {
+            hlist = hurricanes.findByNameContainingOrLocationContaining(search,search);
+        }
+        else {
+            hlist = (List<Hurricane>) hurricanes.findAll();
+        }
         model.addAttribute("hurricanes",hlist);
         return "home";
     }
